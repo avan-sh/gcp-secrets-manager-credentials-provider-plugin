d88dd732bb9302a331a72273e24907e559fa3904
avan-sh <avaneesh.yembadi@tamr.com>
2023-06-20 13:24:21 -0400

    fix: remove password logging

--- src/main/java/io/jenkins/plugins/credentials/gcp/secretsmanager/GcpJsonUsernamePasswordCredentials.java
+++ src/main/java/io/jenkins/plugins/credentials/gcp/secretsmanager/GcpJsonUsernamePasswordCredentials.java
@@ -7,7 +7,6 @@ import com.cloudbees.plugins.credentials.impl.BaseStandardCredentials;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Map;
-//import logging
 import java.util.logging.Logger;
 
 import hudson.util.Secret;
@@ -17,20 +16,15 @@ public class GcpJsonUsernamePasswordCredentials extends BaseStandardCredentials
 
     private static final Logger LOGGER = Logger.getLogger(GcpJsonUsernamePasswordCredentials.class.getName());
 
-    private final Supplier<Secret> jsonSecret;
     private final String username;
     private final String password;
 
     public GcpJsonUsernamePasswordCredentials(String id, String description, Supplier<Secret> jsonSecret) {
         super(id, description);
-        this.jsonSecret = jsonSecret;
         this.username = parseUsernamePassword(jsonSecret)[0];
         this.password = parseUsernamePassword(jsonSecret)[1];
-        LOGGER.info("GcpJsonUsernamePasswordCredentials constructor called");
     }
 
-    // function to parse Json string and return username and password
-    // as key value pair, or as a map
     public String[] parseUsernamePassword(Supplier<Secret> jsonString) {
         String[] usernamePassword = new String[2];
         ObjectMapper mapper = new ObjectMapper();
@@ -40,9 +34,9 @@ public class GcpJsonUsernamePasswordCredentials extends BaseStandardCredentials
             Map<String, Object> map = mapper.readValue(json, Map.class);
             usernamePassword[0] = map.get("username").toString();
             usernamePassword[1] = map.get("password").toString();
-            LOGGER.info("username = " + usernamePassword[0]);
-            LOGGER.info("Password = " + usernamePassword[1]);
+            LOGGER.info("Successfully parsed Json string");
         } catch (JsonProcessingException e) {
+            LOGGER.warning("Error parsing secret string, expecting Json with username and password keys");
             throw new RuntimeException(e);
         }
 
